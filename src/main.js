7201638a6cefbb9fb07bf2d18867700c4462bec9
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-11-16 16:45:59 -0300

    requisito 03 concluido

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { createProductElement } from './helpers/shopFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+
+const sectionProducts = document.querySelector('.products');
+
+const AppendElements = async () => {
+  const productList = await fetchProductsList('computador');
+  productList.forEach((product) => {
+    const productElement = createProductElement(product);
+    sectionProducts.appendChild(productElement);
+  });
+};
+
+AppendElements();
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
