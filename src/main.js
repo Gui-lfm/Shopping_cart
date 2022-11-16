70846fd7d09bef6b5e67aa7dab91d0fac3db3631
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-11-16 17:26:37 -0300

    requisito 04 concluido

--- src/main.js
+++ src/main.js
@@ -5,8 +5,18 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 
 const sectionProducts = document.querySelector('.products');
 
+const isLoading = () => {
+  const loading = document.createElement('div');
+  loading.classList.add('loading');
+  loading.innerText = 'Carregando...';
+  return loading;
+};
+
 const AppendElements = async () => {
+  const loader = isLoading();
+  sectionProducts.appendChild(loader);
   const productList = await fetchProductsList('computador');
+  sectionProducts.removeChild(loader);
   productList.forEach((product) => {
     const productElement = createProductElement(product);
     sectionProducts.appendChild(productElement);
