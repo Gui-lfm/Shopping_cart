32eb995e6f812c73cc7eae203f44e082974c000e
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-11-16 18:22:22 -0300

    requisitos 06 e 07 concluidos

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,11 +1,18 @@
-export const fetchProduct = () => {
-  // seu código aqui
+export const fetchProduct = async (id) => {
+  if (!id) {
+    throw new Error('ID não informado');
+  }
+
+  const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const data = await response.json();
+  return data;
 };
 
 export const fetchProductsList = async (endpoint) => {
   if (!endpoint) {
     throw new Error('Termo de busca não informado');
   }
+
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${endpoint}`);
   const data = await response.json();
   const { results } = data;
--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -2,9 +2,26 @@ import './mocks/fetchSimulator';
 import { fetchProduct } from '../helpers/fetchFunctions';
 import product from './mocks/product';
 
-// implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('FetchProduct é uma função:', () => {
+    expect(typeof fetchProduct).toBe('function');
+  });
+  it('Fetch é chamado ao executar fetchProduct:', async () => {
+    const id = 'MLB1405519561';
+    await fetchProduct(id);
+    expect(fetch).toBeCalled();
+  });
+  it('Fetch é chamado com o endpoint correto ao executar fetchProduct', async () => {
+    const id = 'MLB1405519561';
+    await fetchProduct(id);
+    expect(fetch).toBeCalledWith(`https://api.mercadolibre.com/items/${id}`);
+  });
+  it('Função com o argumento "MLB1405519561" retorna obj igual "product"', async () => {
+    const result = await fetchProduct('MLB1405519561');
+    expect(result).toEqual(product);
+  });
+  it('FetchProduct retorna uma mensagem de erro caso seja chamada sem parâmetro', () => {
+    const result = fetchProduct();
+    expect(result).rejects.toThrow();
   });
 });
