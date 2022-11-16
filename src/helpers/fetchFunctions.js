c50ce4ef3a9a8193bc4e0318c6bd21981cafccc3
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-11-16 16:02:00 -0300

    requisito 1 e 2 concluido

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,14 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (endpoint) => {
+  if (!endpoint) {
+    throw new Error('Termo de busca não informado');
+  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${endpoint}`);
+  const data = await response.json();
+  const { results } = data;
+  return results;
 };
+
+console.log(fetchProduct());
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -4,17 +4,26 @@ import computadorSearch from './mocks/search';
 
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toBeCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const endpoint = 'computador';
+    await fetchProductsList(endpoint);
+    expect(fetch).toBeCalledWith(`https://api.mercadolibre.com/sites/MLB/search?q=${endpoint}`);
   });
 
-  // it('...', () => {
-  // });
+  it('Função fetchProductsList(computador) retorna o objeto esperado', async () => {
+    const returnedValue = await fetchProductsList('computador');
+    expect(returnedValue).toEqual(computadorSearch);
+  });
+  it('Função retorna uma mensagem de erro caso seja chamada sem parâmetro', () => {
+    const result = fetchProductsList();
+    expect(result).rejects.toThrow();
+  });
 });
